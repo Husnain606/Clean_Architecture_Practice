5e56b070cac6c7f833ca210202100d17018a31e1
Waleed Khalid <wbk606@hotmail.com>
2024-09-21 07:10:17 -0700

    remove folders

--- ADBMS/dbs/SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-CREATE TRIGGER trg_Enrollment_Update
-ON Enrollment
-INSTEAD OF UPDATE
-AS
-BEGIN
-    -- Prevent any updates to sid and cid
-    RAISERROR ('Updates to sid and cid columns are not allowed', 16, 1);
-    ROLLBACK;
-END;
--- Node/index.js
+++
@@ -1,6 +0,0 @@
-const http  = require("http")
-const myServer = http.createServer((req,res)   =>  {
-    console.log(req.url)
-    res.end("Hello Node")
-})
-myServer.listen(3000,()=> console.log("Server Started "));
